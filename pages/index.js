9b85ce331baa58f385a3f719624c2894c00d9586
MSK0303 <yoshihara@ltas03.com>
2021-11-06 20:20:06 +0900

    auto deployテスト

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
       <Layout title="Home">
-        <p className="text-4xl">Hello Next.js</p>
+        <p className="text-4xl">Hello Vercel And Next.js</p>
 
       </Layout>
   )
